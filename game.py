8562eb4e6e18ee1187eedc72df633988cbe11ad3
n00b-cat <157589523+n00b-cat@users.noreply.github.com>
2026-01-25 18:28:52 +0100

    vector movmet and collisons
    added movement that moves equal diagonal and collisions to the tiles that are not empty

--- game.py
+++ game.py
@@ -2,10 +2,18 @@ import pygame, random
 
 pygame.init()
 
-WindowWidth, WindowHeight = 640, 480
-player_x = 10
-player_y = 10
-speed = 0.1
+windowWidth, windowHeight = 640, 480
+
+tileSize = 32
+tiles = []
+
+class Tile():
+    def __init__(self, x, y, type, color):
+        self.rect = pygame.Rect(x, y, 32, 32)
+        self.type = type
+        self.color = color
+
+# Make map
 
 ores = [
     "red",
@@ -15,68 +23,71 @@ ores = [
     "white",
 ]
 
-tileSize = 32
-tiles = []
-
-class Tile():
-    def __init__(self, x, y, type, color):
-        self.x = x
-        self.y = y
-        self.type = type
-        self.color = color
-
-# Make map
 for x in range(20):
     for y in range(15):
-        orechance = random.randint(1, 100)
+        oreChance = random.randint(1, 100)
         
-        if orechance > 90:
+        if oreChance > 90:
             color = ores[random.randint(0, len(ores) -1)]
             type = "ore"
         else:
             color = "#8C5E53"
             type = "dirt"
         
-        tiles.append(Tile(x * tileSize,y * tileSize, type, color))
+        tiles.append(Tile(x * 32, y * 32, type, color))
+
 
 # Game window
-window = pygame.display.set_mode([WindowWidth, WindowHeight])
-pygame.display.set_caption("Epic Mining game")
+window = pygame.display.set_mode([windowWidth, windowHeight])
+pygame.display.set_caption("Mining game")
 
 for tile in tiles:
-    if tile.x / 32 <= 2 and tile.y / 32 <= 2:
+    if tile.rect.x <= 64 and tile.rect.y <= 64:
         tile.color = "#4A3A2D"
         tile.type = "empty"
     
-
 running = True
+clock = pygame.time.Clock()
+deltaTime = 0
+
+player = pygame.FRect(30, 30, 28, 28)
+speed = 1
+directionVector = pygame.math.Vector2()
 
 # Gameloop
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player_x -= speed
-    if keys[pygame.K_RIGHT]:
-        player_x += speed
-    if keys[pygame.K_UP]:
-        player_y -= speed
-    if keys[pygame.K_DOWN]:
-        player_y += speed
-    if keys[pygame.K_SPACE]:
-        print(player_x // tileSize, player_y // tileSize)
+    directionVector.x = keys[pygame.K_d] - keys[pygame.K_a]
+    directionVector.y = keys[pygame.K_s] - keys[pygame.K_w]
+
+    if directionVector:
+        directionVector = directionVector.normalize()
+    
+    player.x += directionVector.x * speed
+
+    for tile in tiles:
+        if player.colliderect(tile.rect) and tile.type != "empty":
+            player.x -= directionVector.x * speed
+
+    player.y += directionVector.y * speed
+
+    for tile in tiles:
+        if player.colliderect(tile.rect) and tile.type != "empty":
+            player.y -= directionVector.y * speed
 
     window.fill("black")
 
     for tile in tiles:
-        pygame.draw.rect(window, tile.color, (tile.x, tile.y, tileSize, tileSize)) 
+        pygame.draw.rect(window, tile.color, tile.rect) 
         
-    pygame.draw.rect(window, (73, 119, 238), (player_x, player_y, 32, 32)) 
+    pygame.draw.rect(window, "blue", player) 
 
     pygame.display.update()
+
+    deltaTime = clock.tick(60) / 1000
 
 pygame.quit()
