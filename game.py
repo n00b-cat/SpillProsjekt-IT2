afcbe0e63d2f581adc07b6d019e222c3affea361
n00b-cat <157589523+n00b-cat@users.noreply.github.com>
2026-01-26 14:34:10 +0100

    Mining
    if you move into a tile you can now mine it

--- game.py
+++ game.py
@@ -2,59 +2,49 @@ import pygame, random
 
 pygame.init()
 
+# Game window
 windowWidth, windowHeight = 640, 480
+window = pygame.display.set_mode([windowWidth, windowHeight])
+pygame.display.set_caption("Mining game")
 
+# Make map
 tileSize = 32
 tiles = []
 
+image = pygame.image.load("tilemap.png")
+
 class Tile():
-    def __init__(self, x, y, type, color):
+    def __init__(self, x, y, type):
         self.rect = pygame.Rect(x, y, 32, 32)
         self.type = type
-        self.color = color
-
-# Make map
-
-ores = [
-    "red",
-    "green",
-    "yellow",
-    "blue",
-    "white",
-]
 
 for x in range(20):
     for y in range(15):
         oreChance = random.randint(1, 100)
         
         if oreChance > 90:
-            color = ores[random.randint(0, len(ores) -1)]
             type = "ore"
         else:
-            color = "#8C5E53"
             type = "dirt"
         
-        tiles.append(Tile(x * 32, y * 32, type, color))
+        tiles.append(Tile(x * 32, y * 32, type))
 
-
-# Game window
-window = pygame.display.set_mode([windowWidth, windowHeight])
-pygame.display.set_caption("Mining game")
 
 for tile in tiles:
     if tile.rect.x <= 64 and tile.rect.y <= 64:
-        tile.color = "#4A3A2D"
         tile.type = "empty"
     
+player = pygame.FRect(30, 30, 20, 20)
+speed = 1.3
+directionVector = pygame.math.Vector2()
+lastMine = 0
+mineColldown = 500
+
+# Gameloop
 running = True
 clock = pygame.time.Clock()
 deltaTime = 0
 
-player = pygame.FRect(30, 30, 28, 28)
-speed = 1
-directionVector = pygame.math.Vector2()
-
-# Gameloop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -71,19 +61,39 @@ while running:
 
     for tile in tiles:
         if player.colliderect(tile.rect) and tile.type != "empty":
-            player.x -= directionVector.x * speed
+            currentTime = pygame.time.get_ticks()
+
+            if currentTime - lastMine >= mineColldown:
+                lastMine = currentTime
+                player.x -= (directionVector.x * speed) * 6
+                tile.type = "empty"
+            else:
+                player.x -= directionVector.x * speed
+                
 
     player.y += directionVector.y * speed
 
     for tile in tiles:
         if player.colliderect(tile.rect) and tile.type != "empty":
-            player.y -= directionVector.y * speed
+            currentTime = pygame.time.get_ticks()
 
+            if currentTime - lastMine >= mineColldown:
+                lastMine = currentTime
+                player.y -= (directionVector.y * speed) * 6
+                tile.type = "empty"
+            else:
+                player.y -= directionVector.y * speed
+    # Draw
     window.fill("black")
 
     for tile in tiles:
-        pygame.draw.rect(window, tile.color, tile.rect) 
-        
+        if (tile.type == "empty"):
+            window.blit(image, tile.rect, (0, 0, 32, 32))
+        elif (tile.type == "dirt"):
+            window.blit(image, tile.rect, (32, 0, 64, 32))
+        elif (tile.type == "ore"):
+            window.blit(image, tile.rect, (64, 0, 96, 32))
+
     pygame.draw.rect(window, "blue", player) 
 
     pygame.display.update()
