2055e318f5ced321ee5743915119a4328d17b8da
n00b-cat <157589523+n00b-cat@users.noreply.github.com>
2026-01-30 09:49:19 +0100

    work in progress

--- game.py
+++ game.py
@@ -27,7 +27,7 @@ for x in range(20):
             type = "ore"
             value = 20
         else:
-            type = "stone"
+            type = "dirt"
             value = 1
         
         tiles.append(Tile(x * 32, y * 32, type, value))
@@ -38,14 +38,19 @@ for tile in tiles:
         tile.type = "empty"
 
 # Player stats 
-player = pygame.FRect(30, 30, 20, 20)
-playerSpeed = 1.3
-directionVector = pygame.math.Vector2()
-playerImage = pygame.image.load("player.png")
 
-lastMine = 0
-mineColldown = 1000
-money = 0
+class Player():
+    def __init__(self, speed):
+        self.rect = pygame.FRect(30, 30, 20, 20)
+        self.speed = speed
+        self.directionVector = pygame.math.Vector2()
+        self.image = pygame.image.load("player.png")
+        
+        self.lastMine = 0
+        self.mineColldown = 1000
+        self.money = 0
+
+player = Player(1.3)
 
 # Font
 font = pygame.font.Font('Micro5-Regular.ttf', 32)
@@ -63,6 +68,26 @@ running = True
 clock = pygame.time.Clock()
 deltaTime = 0
 
+def collision(tiles, axis):
+    for tile in tiles:
+        if player.rect.colliderect(tile.rect) and tile.type != "empty":
+            currentTime = pygame.time.get_ticks()
+
+            if currentTime - player.lastMine >= player.mineColldown:
+                player.lastMine = currentTime
+                if axis == "x":
+                    player.rect.x -= (player.directionVector.x * player.speed) * 6
+                elif axis == "y":
+                    player.rect.y -= (player.directionVector.y * player.speed) * 6
+
+                tile.type = "empty"
+                player.money += tile.value
+            else:
+                if axis == "x":
+                    player.rect.x -= player.directionVector.x * player.speed
+                elif axis == "y":
+                    player.rect.y -= player.directionVector.y * player.speed
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,61 +102,57 @@ while running:
                 else:
                     shopOpen = True
                 
-            elif (upgradeTextRect.collidepoint(mousePosition)) and event.button == 1 and money >= upgradePrice:
-                print("test")
-                money -= upgradePrice
-                upgradePrice += upgradePrice * 1.05
-                mineColldown -= 100
+            elif (upgradeTextRect.collidepoint(mousePosition)) and event.button == 1 and player.money >= upgradePrice:
+                player.money -= upgradePrice
+                upgradePrice = upgradePrice * 1.05
+                player.mineColldown -= 100
 
     keys = pygame.key.get_pressed()
-    directionVector.x = keys[pygame.K_d] - keys[pygame.K_a]
-    directionVector.y = keys[pygame.K_s] - keys[pygame.K_w]
+    player.directionVector.x = keys[pygame.K_d] - keys[pygame.K_a]
+    player.directionVector.y = keys[pygame.K_s] - keys[pygame.K_w]
 
-    if directionVector:
-        directionVector = directionVector.normalize()
+    if player.directionVector:
+        player.directionVector = player.directionVector.normalize()
+
+    # Surrounding tiles
+    # cordinateX = int(player.rect.x // 32)
+    # cordinateY = int(player.rect.y // 32)
+
+    # for y in range(cordinateY -1, cordinateY + 2):
+    #     for x in range(cordinateX -1, cordinateX + 2):
+    #         print(f"x:{x}, y:{y}")
     
-    player.x += directionVector.x * playerSpeed
-
-    for tile in tiles:
-        if player.colliderect(tile.rect) and tile.type != "empty":
-            currentTime = pygame.time.get_ticks()
-
-            if currentTime - lastMine >= mineColldown:
-                lastMine = currentTime
-                player.x -= (directionVector.x * playerSpeed) * 6
-                tile.type = "empty"
-                money += tile.value
-            else:
-                player.x -= directionVector.x * playerSpeed
+    player.rect.x += player.directionVector.x * player.speed
+    collision(tiles, "x")
                 
-
-    player.y += directionVector.y * playerSpeed
+    player.rect.y += player.directionVector.y * player.speed
+    collision(tiles, "y")
 
     for tile in tiles:
-        if player.colliderect(tile.rect) and tile.type != "empty":
+        if player.rect.colliderect(tile.rect) and tile.type != "empty":
             currentTime = pygame.time.get_ticks()
 
-            if currentTime - lastMine >= mineColldown:
-                lastMine = currentTime
-                player.y -= (directionVector.y * playerSpeed) * 6
+            if currentTime - player.lastMine >= player.mineColldown:
+                player.lastMine = currentTime
+                player.rect.y -= (player.directionVector.y * player.speed) * 6
                 tile.type = "empty"
-                money += tile.value
+                player.money += tile.value
             else:
-                player.y -= directionVector.y * playerSpeed
+                player.rect.y -= player.directionVector.y * player.speed
     # Draw
     window.fill("black")
 
     for tile in tiles:
         if (tile.type == "empty"):
-            window.blit(tileMapImage, tile.rect, (0, 0, 32, 32))
-        elif (tile.type == "stone"):
+            window.blit(tileMapImage, tile.rect, (160, 32, 192, 64))
+        elif (tile.type == "dirt"):
             window.blit(tileMapImage, tile.rect, (32, 0, 64, 32))
         elif (tile.type == "ore"):
-            window.blit(tileMapImage, tile.rect, (64, 0, 96, 32))
+            window.blit(tileMapImage, tile.rect, (32, 96, 64, 128))
 
-    window.blit(playerImage, player, (0, 0, 24, 24))
+    window.blit(player.image, player, (0, 0, 24, 24))
 
-    moneyCounter = font.render(f"Money: ${money}", True, "white", "black")
+    moneyCounter = font.render(f"Money: ${player.money}", True, "white", "black")
     window.blit(moneyCounter, (10, 10))
     
     window.blit(shopText, (560, 10))
